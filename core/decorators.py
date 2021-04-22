8eaae95b50ce7a368113b7d401e08fa9ea84747c
Pashs-ba <pashs.ba@gmail.com>
2021-04-22 20:54:30 +0300

    create user login

--- core/decorators.py
+++ core/decorators.py
@@ -4,8 +4,9 @@ from django.shortcuts import render, redirect
 
 def admin_only(view_func):
     def wrapper(request, *args, **kwargs):
-        if request.user.is_staff:
+        if request.user.is_authenticated and request.user.is_staff:
             return view_func(request, *args, **kwargs)
         messages.error(request, 'Only admins can access this page')
         return redirect('homepage.html')
     return wrapper
+
