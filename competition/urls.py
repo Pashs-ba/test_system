ff9248c72242b8f49b95390bd1bc1e7b57a17e7b
Pashs-ba <pashs.ba@gmail.com>
2021-05-31 18:53:23 +0300

    load ans alfa

--- competition/urls.py
+++ competition/urls.py
@@ -2,5 +2,6 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path('<int:pk>', competition_page, name='competition_page')
+    path('<int:pk>', competition_page, name='competition_page'),
+    path('<int:pk>/load_ans', load_ans, name='load_ans')
 ]
--- competition/views.py
+++ competition/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from core.models import Competitions, Contests
 from core.utils import competition_status
+from django.conf import settings
 
 
 def competition_page(request, pk):
@@ -10,3 +11,17 @@ def competition_page(request, pk):
     }
     context.update({'status': competition_status(competition)})
     return render(request, 'competition.html', context=context)
+
+
+def load_ans(request, pk):
+    competition = Competitions.objects.get(pk=pk)
+    context = {
+        'competition': competition,
+        'langs': settings.ACCEPTABLE_LANGUAGES
+    }
+    if request.GET.get('contest', ''):
+        contest = Contests.objects.get(pk=request.GET.get('contest', ''))
+        context.update({
+            'contest': contest
+        })
+    return render(request, 'load_ans.html', context)
