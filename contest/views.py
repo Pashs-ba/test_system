96f927ea7c054b983c1763e8cb3d67163730a064
Pashs-ba <pashs.ba@gmail.com>
2021-06-25 17:44:05 +0300

    solutions table

--- contest/views.py
+++ contest/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from core.models import Contests
+from core.models import Contests, Solutions
 
 
 def contests(request, pk):
     contest = Contests.objects.get(pk=pk)
     examples = contest.test_set.filter(is_example=True)
+    solutions = Solutions.objects.filter(contest=contest, user=request.user).order_by('date')
     return render(request, 'contest_page.html', {
         'contest': contest,
-        'examples': examples
+        'examples': examples,
+        'solutions': solutions,
+        'bad': ['TL', 'ML', 'WA', 'CE'],
     })
