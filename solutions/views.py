54ce20bbd69548862a8a9716d4b7d1d1183b8f34
Pashs-ba <pashs.ba@gmail.com>
2021-07-19 18:01:30 +0300

    IT WORKS!

--- solutions/views.py
+++ solutions/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from core.models import Solutions
+import re
+
+
+def change(matchobj):
+    return '\\' + matchobj.groups()[0]
 
 
 def result_page(request, pk):
     with open(Solutions.objects.get(pk=pk).file_name, 'r') as f:
-        code = f.read()
+        code = f.read().replace('\n\n', '\n')
+
+    code = str(re.sub(r"([.*+?^${}()\"\'\\])", change, code))
+    code = code.replace('\n', '\\n')
+    print(code)
     return render(request, 'result.html', context={'solution': Solutions.objects.get(pk=pk),
-                                                   'code': code})
+                                                   'code': code,
+                                                   'bad': ['TL', 'ML', 'WA', 'CE']})
