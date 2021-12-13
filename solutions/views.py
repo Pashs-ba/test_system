92c02bfe4930ff1064755d55b1f58a61ae5aa7a9
Pashs-ba <pashs.ba@gmail.com>
2021-12-13 13:51:07 +0300

    fix solution bug

--- solutions/views.py
+++ solutions/views.py
@@ -6,16 +6,14 @@ import re
 def change(matchobj):
     return '\\' + matchobj.groups()[0]
 
-# TODO fix this shit
-
 def result_page(request, pk, comp_pk):
-    with open(Solutions.objects.get(pk=pk).file_name, 'r') as f:
+    with open('media/'+Solutions.objects.get(pk=pk).file_name, 'r') as f:
         code = f.read().replace('\n\n', '\n')
 
     code = str(re.sub(r"([.*+?^${}()\"\'\\])", change, code))
     code = code.replace('\n', '\\n')
     p = Solutions.objects.get(pk=pk).contest.pk
-    return render(request, 'result.html', context={'solution': Solutions.objects.get(pk=pk),
+    return render(request, 'result_sol.html', context={'solution': Solutions.objects.get(pk=pk),
                                                    'code': code,
                                                    'bad': ['TL', 'ML', 'WA', 'CE'],
                                                    'back': 'К задаче',
