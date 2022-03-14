d9e32596ab42370eb2e0c70c5ae4821f148aaac7
Pashs-ba <pashs.ba@gmail.com>
2022-03-14 16:05:44 +0300

    fix auto close competition

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [path('make_example', make_example, name='make_example'),
                path('status/<int:pk>', is_open, name='is_open'),
                path('solution', get_status, name='solution_status'),
-               path('save_ans/<int:q_pk>', set_ans, name="save_ans")]
+               path('save_ans/<int:q_pk>', set_ans, name="save_ans"),
+               path('is_close/<int:pk>', is_close, name="is_close")]
