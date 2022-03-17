adbafb65342345ab61c9e56eb86f2814a6fc3613
Pashs-ba <pashs.ba@gmail.com>
2022-03-17 12:41:19 +0300

    a

--- core/utils.py
+++ core/utils.py
@@ -28,37 +28,37 @@ def make_xl(request, competition, id):
     al = group.users.all().order_by('username')
     qa = comp.questions.all().order_by('name')
     with open(settings.BASE_DIR/f'media/{id}.txt', 'w', encoding="utf-8") as f:
-        f.write('Привет\n')
-        f.write(qa[0].name)
-        print(qa[0].name)
-        # for quest in range(len(qa)): 
-        #     f.write(f'{qa[quest].name} ')
-        # f.write('\n')
-        # print()
-        # for user in range(len(al)):
-        #     # ws.cell(row=user+2, column=1).value = al[user].username
-        #     f.write(f'{al[user].username} ')
-        #     for quest in range(len(qa)):
-        #         # print(qa[quest].name)
-        #         # ws.cell(row=1, column=quest+2).value = qa[quest].name
-        #         # f.write(f'{qa[quest].name} ')
-        #         a = QuestionAns.objects.filter(user=al[user].pk, question=qa[quest].pk)
-        #         # ws.cell(row=user+2, column=quest+2, value="0")
-        #         if a:
-        #             if a[0].result:
-        #                 if a[0].time:
-        #                     delta = a[0].time-comp.start_time
-        #                     f.write('+ ')
-        #                     # ws.cell(row=user+2, column=quest+2, value=f"+ {delta.total_seconds()//60}")
-        #             else:
-        #                 if a[0].time:
-        #                     delta = a[0].time-comp.start_time
-        #                     f.write('- ')
-        #         else:
-        #             f.write('0 ')
-        #     f.write('\n')
-                            # ws.cell(row=user+2, column=quest+2, value=f"- {delta.total_seconds()//60}")
-    # wb.save(str(settings.BASE_DIR / f'media/{id}.xlsx'))
+        # f.write('Привет\n')
+        # f.write(qa[0].name)
+        # print(qa[0].name)
+        for quest in range(len(qa)): 
+            f.write(f'{qa[quest].name} ')
+        f.write('\n')
+        print()
+        for user in range(len(al)):
+            # ws.cell(row=user+2, column=1).value = al[user].username
+            f.write(f'{al[user].username} ')
+            for quest in range(len(qa)):
+                # print(qa[quest].name)
+                # ws.cell(row=1, column=quest+2).value = qa[quest].name
+                # f.write(f'{qa[quest].name} ')
+                a = QuestionAns.objects.filter(user=al[user].pk, question=qa[quest].pk)
+                # ws.cell(row=user+2, column=quest+2, value="0")
+                if a:
+                    if a[0].result:
+                        if a[0].time:
+                            delta = a[0].time-comp.start_time
+                            f.write('+ ')
+                            # ws.cell(row=user+2, column=quest+2, value=f"+ {delta.total_seconds()//60}")
+                    else:
+                        if a[0].time:
+                            delta = a[0].time-comp.start_time
+                            f.write('- ')
+                else:
+                    f.write('0 ')
+            f.write('\n')
+                            ws.cell(row=user+2, column=quest+2, value=f"- {delta.total_seconds()//60}")
+    wb.save(str(settings.BASE_DIR / f'media/{id}.xlsx'))
 
 
 def competition_status(competition: Competitions):
