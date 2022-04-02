2735cf2a0168867c7ad593443396e5cab7aeebeb
Pashs-ba <pashs.ba@gmail.com>
2022-04-02 16:03:58 +0300

    fix decorator

--- core/decorators.py
+++ core/decorators.py
@@ -7,6 +7,6 @@ def admin_only(view_func):
         if request.user.is_authenticated and (request.user.is_staff or request.user.is_teacher):
             return view_func(request, *args, **kwargs)
         messages.error(request, 'Only admins can access this page')
-        return redirect('homepage.html')
+        return redirect('homepage')
     return wrapper
 
