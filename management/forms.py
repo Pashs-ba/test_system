f55be10291d27c009cd2330de9337fed30998a80
Pashs-ba <pashs.ba@gmail.com>
2022-04-03 19:38:18 +0300

    fix

--- management/forms.py
+++ management/forms.py
@@ -31,8 +31,8 @@ class TeacherCompetitionForm(ModelForm):
     class Meta:
         model = Competitions
         fields = ['name', 'description', 'is_unlimited', 'start_time', 'end_time','is_visible_result', 'is_simulator', "is_final", 'questions']
-        widgets = {'start_time': DateTimeInput(attrs={'type': 'datetime-local', 'class': 'time'}, format="%Y-%m-%dT%H:%M"),
-                'end_time': DateTimeInput(attrs={'type': 'datetime-local', 'class': 'time'}, format="%Y-%m-%dT%H:%M"),
+        widgets = {'start_time': DateTimeInput(attrs={'type': 'datetime-local', 'class': 'time', 'disabled':'disabled'}, format="%Y-%m-%dT%H:%M"),
+                'end_time': DateTimeInput(attrs={'type': 'datetime-local', 'class': 'time', 'disabled':'disabled'}, format="%Y-%m-%dT%H:%M"),
                 'is_unlimited': CheckboxInput(attrs={'onchange': 'time_close()', 'id': 'unlim'}),
                 'is_visible_result': CheckboxInput(),
                 'is_simulator': CheckboxInput(),
