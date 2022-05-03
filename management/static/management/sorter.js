c0c820024403488745c677d3be117875c19a2816
Pashs-ba <pashs.ba@gmail.com>
2022-05-03 10:48:20 +0300

    add sort to all manage pages

--- management/static/management/sorter.js
+++ management/static/management/sorter.js
@@ -32,22 +32,24 @@ function sort_table(el, reverse){
         rows = table.rows;
         for (i = 0; i < (rows.length - 1); i++) {
         shouldSwitch = false;
-        x = rows[i].getElementsByTagName("TD")[n];
-        y = rows[i + 1].getElementsByTagName("TD")[n];
+        x = rows[i].getElementsByTagName("TH")[n];
+        y = rows[i + 1].getElementsByTagName("TH")[n];
         if (dir == "asc") {
             if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
             shouldSwitch = true;
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
             break;
             }
         } else if (dir == "desc") {
             if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
             shouldSwitch = true;
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
             break;
             }
         }
         }
         if (shouldSwitch) {
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        // rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
         switching = true;
         switchcount ++;
         } else {
